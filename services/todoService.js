4b5a90db9133bb3736b0fd04806f70a351e36f42
Anuj Shrestha <anujshrestha@lftechnology.com>
2018-05-16 18:33:26 +0545

    CA-3 Add checkbox to check todos

--- services/todoService.js
+++ services/todoService.js
@@ -10,8 +10,8 @@ app.service('todoService', function () {
         { id: 3, task: 'Read books', isCompleted: false },
         { id: 4, task: 'Do Homework', isCompleted: false },
         { id: 5, task: 'Fire dave from work', isCompleted: false },
-        { id: 6, task: 'Buy socks', isCompleted: false },
-        { id: 7, task: 'Get a jar of water', isCompleted: false },
+        { id: 6, task: 'Buy socks', isCompleted: true },
+        { id: 7, task: 'Get a jar of water', isCompleted: true },
         { id: 8, task: 'Become expert in one stack', isCompleted: false },
     ];
 });
